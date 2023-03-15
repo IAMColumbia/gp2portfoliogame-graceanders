4bfed312b3c8a3ce42f790a2acfbe1036522a42b
Grace Anders <grace.anders2002@gmail.com>
2023-03-14 19:30:25 -0500

    Adding my Project

---
+++ src/FinalGame/GameConsolePlayer.cs
@@ -0,0 +1,48 @@
+﻿using MonoGameLibrary.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalGame
+{
+    internal class GameConsolePlayer : Player
+    {
+        GameConsole console;
+
+        public GameConsolePlayer()
+        {
+            this.console = null;
+        }
+
+        public GameConsolePlayer(GameConsole console)
+        {
+            this.console = console;
+        }
+
+        public override void Log(string s)
+        {
+            if(console != null)
+            {
+                console.GameConsoleWrite(s);
+            }
+            else
+            {
+                base.Log(s);
+            }
+        }
+
+        public virtual void Log(string DebugKey, string DebugValue)
+        {
+            if (console != null)
+            {
+                console.Log(DebugKey, DebugValue);
+            }
+            else
+            {
+                base.Log(DebugKey + ":" + DebugValue);
+            }
+        }
+    }
+}
---
+++ src/FinalGame/GameManager.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalGame
+{
+    public class GameManager : DrawableGameComponent
+    {
+        SpriteBatch sb;
+
+        Game g;
+
+        PlayableCharacter playableCharacter;
+        GridManager gridManager;
+
+        public GameManager(Game game) : base(game)
+        {
+            g = game;
+        }
+
+        internal GameManager(Game game, PlayableCharacter p, GridManager gm) : base(game)
+        {
+            g = game;
+            playableCharacter= p;
+            gridManager = gm;
+        }
+
+        protected override void LoadContent()
+        {
+            LoadGameElements();
+            base.LoadContent();
+        }
+
+        private void LoadGameElements()
+        {
+            sb = new SpriteBatch(this.Game.GraphicsDevice);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            HandleInput(gameTime);
+            playableCharacter.Update(gameTime);
+            gridManager.CheckCollision(playableCharacter);
+
+            base.Update(gameTime);
+        }
+
+
+        public void HandleInput(GameTime gameTime)
+        {
+
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            sb.Begin();
+
+            sb.End();
+
+            base.Draw(gameTime);
+        }
+
+    }
+}
