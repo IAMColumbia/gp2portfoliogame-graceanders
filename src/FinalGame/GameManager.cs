59a48f3609ebda49732461147c0a4a6c4c024c4e
Grace Anders <grace.anders2002@gmail.com>
2023-03-19 10:49:39 -0500

    Revert "Added Simple Time Mechanism"
    This reverts commit 0f028f71b40df77058b9144b503cc2df2d17b208.

--- src/FinalGame/GameManager.cs
+++ src/FinalGame/GameManager.cs
@@ -14,13 +14,8 @@ namespace FinalGame
 
         Game g;
 
-        SpriteFont font;
-
         PlayableCharacter playableCharacter;
         GridManager gridManager;
-
-        float CurrentTime, DayTime, DayDuration;
-        int Day;
 
         public GameManager(Game game) : base(game)
         {
@@ -43,9 +38,6 @@ namespace FinalGame
         private void LoadGameElements()
         {
             sb = new SpriteBatch(this.Game.GraphicsDevice);
-            font = this.Game.Content.Load<SpriteFont>("Arial");
-            Day = 1;
-            DayDuration = 10;//Going to change to 300 (5 min)
         }
 
         public override void Update(GameTime gameTime)
@@ -53,34 +45,19 @@ namespace FinalGame
             HandleInput(gameTime);
             playableCharacter.Update(gameTime);
             gridManager.CheckCollision(playableCharacter);
-            UpdateTime(gameTime);
 
             base.Update(gameTime);
         }
 
-
-        public void UpdateTime(GameTime gameTime) 
-        {
-            CurrentTime += (float)gameTime.ElapsedGameTime.TotalSeconds;
-            DayTime += (float)gameTime.ElapsedGameTime.TotalSeconds;
-            if (DayTime >= DayDuration) 
-            {
-                Day++;
-                DayTime = 0;
-            }
-        }
 
         public void HandleInput(GameTime gameTime)
         {
 
         }
 
-        Vector2 TimeLocation = new Vector2(10, 10);
         public override void Draw(GameTime gameTime)
         {
             sb.Begin();
-
-            sb.DrawString(font, $"Total Time: {(int)CurrentTime} Day Time: {(int)DayTime} Day: {Day}", TimeLocation, Color.White);
 
             sb.End();
 
